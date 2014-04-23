dd174b4beb39d285bfe5a43675418d5d05f63204
japacible <japacible@gmail.com>
2014-04-23 00:54:57 -0700

    new views, not connected to each other

--- app/src/main/java/com/dashtricks/pakistan/app/CreateScenarioActivity.java
+++ app/src/main/java/com/dashtricks/pakistan/app/CreateScenarioActivity.java
@@ -1,22 +1,16 @@
 package com.dashtricks.pakistan.app;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class StartActivity extends ActionBarActivity {
+public class CreateScenarioActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_create_scenario);
     }
 
 
@@ -24,7 +18,7 @@ public class StartActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start, menu);
+        getMenuInflater().inflate(R.menu.create_scenario, menu);
         return true;
     }
 
@@ -40,19 +34,4 @@ public class StartActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_start, container, false);
-            return rootView;
-        }
-    }
 }
