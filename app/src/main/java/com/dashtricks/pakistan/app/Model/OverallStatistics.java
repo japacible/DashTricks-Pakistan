31c1bb5dd09e47f191f8e1d615d734b768d19f0a
japacible <japacible@gmail.com>
2014-05-26 00:17:05 -0700

    add basic overall statistics interface and activity

---
+++ app/src/main/java/com/dashtricks/pakistan/app/Model/OverallStatistics.java
@@ -0,0 +1,53 @@
+package com.dashtricks.pakistan.app.Model;
+
+/**
+ * Created by japacible on 5/25/14.
+ */
+public class OverallStatistics {
+    private int totalFacilitiesCount;
+    private int attentionFacilitiesCount;
+    private int totalPopulation;
+    private int coveredPopulation;
+    private double percentageCapacity;
+    private int totalRequiredCapacity;
+    private int totalCoveredCapacity;
+
+    public OverallStatistics() {
+        totalFacilitiesCount = 0;
+        attentionFacilitiesCount = 0;
+        totalPopulation = 0;
+        coveredPopulation = 0;
+
+        percentageCapacity = 0;
+        totalRequiredCapacity = 0;
+        totalCoveredCapacity = 0;
+    }
+
+    public int getTotalFacilitiesCount() {
+        return totalFacilitiesCount;
+    }
+
+    public int getAttentionFacilitiesCount() {
+        return attentionFacilitiesCount;
+    }
+
+    public int getTotalPopulation() {
+        return totalPopulation;
+    }
+
+    public int getCoveredPopulation() {
+        return coveredPopulation;
+    }
+
+    public double getPercentageCapacity() {
+        return percentageCapacity;
+    }
+
+    public int getTotalRequiredCapacity() {
+        return totalRequiredCapacity;
+    }
+
+    public int getTotalCoveredCapacity() {
+        return totalCoveredCapacity;
+    }
+}
