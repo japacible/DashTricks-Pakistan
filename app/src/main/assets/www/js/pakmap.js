dfd8a57ae334f21e2acb7f4867c0225020ba705b
japacible <japacible@gmail.com>
2014-05-29 21:27:48 -0700

    replace tabs with spaces

--- app/src/main/assets/www/js/pakmap.js
+++ app/src/main/assets/www/js/pakmap.js
@@ -1,74 +1,74 @@
 function randHeat() {
-	return "heat-" + ( 6 - Math.round(Math.pow(Math.random(), 3) * 6));
+    return "heat-" + ( 6 - Math.round(Math.pow(Math.random(), 3) * 6));
 }
 
 var width = 640,
-	height = 552;
+    height = 552;
 
 var zoom = d3.behavior.zoom()
-	.scaleExtent([1, 10])
-	.on("zoom", zoomed);
+    .scaleExtent([1, 10])
+    .on("zoom", zoomed);
 
 var drag = d3.behavior.drag()
-	.origin(function(d) { return d; })
-	.on("dragstart", dragstarted)
-	.on("drag", dragged)
-	.on("dragend", dragended);
+    .origin(function(d) { return d; })
+    .on("dragstart", dragstarted)
+    .on("drag", dragged)
+    .on("dragend", dragended);
 
 var projection = d3.geo.albers()
-	.center([0, 30])
-	.rotate([-70, 0])
-	.parallels([23, 37])
-	.scale(4 * height)
-	.translate([width * 0.5, height * 0.53]);
+    .center([0, 30])
+    .rotate([-70, 0])
+    .parallels([23, 37])
+    .scale(4 * height)
+    .translate([width * 0.5, height * 0.53]);
 
 var path = d3.geo.path()
-	.projection(projection)
-	.pointRadius(2);
+    .projection(projection)
+    .pointRadius(2);
 
 var svg = d3.select("body").append("svg")
-		.attr("width", width)
-		.attr("height", height)
-	.append("g")
-		.call(zoom);
+        .attr("width", width)
+        .attr("height", height)
+    .append("g")
+        .call(zoom);
 
 var rect = svg.append("rect")
-	.attr("width", width)
-	.attr("height", height)
-	.style("fill", "none")
-	.style("pointer-events", "all");
+    .attr("width", width)
+    .attr("height", height)
+    .style("fill", "none")
+    .style("pointer-events", "all");
 
 var container = svg.append("g");
 
 d3.json("pakistan.json", function(error, pak) {
-	var provinces = topojson.feature(pak, pak.objects.provinces),
-	districts = topojson.feature(pak, pak.objects.subdistricts);
+    var provinces = topojson.feature(pak, pak.objects.provinces),
+    districts = topojson.feature(pak, pak.objects.subdistricts);
 
-	container.selectAll(".district")
-			.data(districts.features)
-		.enter().append("path")
-			.attr("class", function(d) { return "district " + d.properties.name3.replace(/[ .]/g, "") + " " + randHeat(); })
-			.attr("d", path);
+    container.selectAll(".district")
+            .data(districts.features)
+        .enter().append("path")
+            .attr("class", function(d) { return "district " + d.properties.name3.replace(/[ .]/g, "") + " " + randHeat(); })
+            .attr("d", path);
 
-	container.append("path")
-		.datum(topojson.mesh(pak, pak.objects.subdistricts, function(a, b) { return a !== b; })) // return a !== b; for tangential borders only
-		.attr("d", path)
-		.attr("class", "district-boundary");
+    container.append("path")
+        .datum(topojson.mesh(pak, pak.objects.subdistricts, function(a, b) { return a !== b; })) // return a !== b; for tangential borders only
+        .attr("d", path)
+        .attr("class", "district-boundary");
 
-	container.append("path")
-		.datum(topojson.mesh(pak, pak.objects.provinces, function(a, b) { return a !== b; })) // return a !== b; for tangential borders only
-		.attr("d", path)
-		.attr("class", "province-boundary");
+    container.append("path")
+        .datum(topojson.mesh(pak, pak.objects.provinces, function(a, b) { return a !== b; })) // return a !== b; for tangential borders only
+        .attr("d", path)
+        .attr("class", "province-boundary");
 
-	svg.selectAll(".district").on("click", function() {
-		svg.selectAll(".selected")
-			.classed("selected", false)
-			.style("fill", "");
+    svg.selectAll(".district").on("click", function() {
+        svg.selectAll(".selected")
+            .classed("selected", false)
+            .style("fill", "");
 
-		d3.select(this)
-			.classed("selected", true)
-			.style("fill", "#4cc");
-	});
+        d3.select(this)
+            .classed("selected", true)
+            .style("fill", "#4cc");
+    });
 });
 
 function zoomed() {
