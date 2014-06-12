816582e868edf133faa6041f03d07be73434cc62
japacible <japacible@gmail.com>
2014-06-12 01:38:32 -0700

    canned data to be passed in as a string

--- app/src/main/java/com/dashtricks/pakistan/app/Utilities/JsonObjects.java
+++ app/src/main/java/com/dashtricks/pakistan/app/Utilities/JsonObjects.java
@@ -4,18 +4,35 @@ package com.dashtricks.pakistan.app.Utilities;
  * Created by japacible on 5/29/14.
  */
 public class JsonObjects {
-    public final static String URGENT_NEED =
-      "{\"Okay\":1523,\"Needs Attention\":605}";
+    public final static String EXPLORE_MAP_DATA =
+        "";
 
-    public final static String FRIDGE_AGE =
-      "";
+    public final static String FRIDGE_AGE_CATEGORIES =
+        "[ \"0-4\", \"4-8\", \"8-12\", \"12-16\", \"16-20\", \"20-24\","
+        + "\"24-28\", \"28-32\", \"32-36\", \"36-40\" ]";
 
-    public final static String PUNJAB =
-      "";
+    public final static String FRIDGE_AGE_DATA =
+        "[ \"24\", \"45\", \"50\", \"23\", \"15\", \"33\", \"29\", \"14\", \"37\", \"21\" ]";
 
-    public final static String EXPLORATION_MAP =
-      "";
+    public final static String PUNJAB_DATA =
+        "[ \"0.24\", \"0.31\", \"0.54\", \"0.12\", \"0.53\", \"0.23\", \"0.29\", \"0.91\","
+        + "\"0.62\", \"0.24\", \"0.54\", \"0.44\", \"0.62\", \"0.23\", \"0.41\", \"0.44\","
+        + "\"0.23\", \"0.15\", \"0.5\", \"0.41\", \"0.34\", \"0.43\", \"0.93\", \"0.05\", \"0.99\","
+        + "\"0.62\", \"0.55\", \"0.67\", \"0.28\", \"0.21\", \"0.12\", \"0.62\", \"0.23\","
+        + "\"0.12\", \"0.12\", \"0.72\" ]";
 
-    public final static String SIMULATION_MAP =
-      "";
+    public final static String PUNJAB_DISTRICTS =
+        "[ \"Attock\", \"Bahawalnagar\", \"Bahawalpur\", \"Bhakkar\", \"Chakwal\", \"Chiniot\","
+        + "\"Dera Ghazi Khan\", \"Faisalabad\", \"Gujranwala\", \"Gujrat\", \"Hafizabad\","
+        + "\"Jhang\", \"Jhelum\", \"Kasur\", \"Khanewal\", \"Khushab\", \"Lahore\", \"Layyah\","
+        + "\"Lodhran\", \"Mandi Bahauddin\", \"Mianwali\", \"Multan\", \"Muzaffargarh\","
+        + "\"Narowal\", \"Nankana Sahib\", \"Okara\", \"Pakpattan\", \"Rahim Yar Khan\","
+        + "\"Rajanpur\", \"Rawalpindi\", \"Sahiwal\", \"Sargodha\", \"Sheikhupura\", \"Sialkot\","
+        + "\"Toba Tek Singh\", \"Vehari\" ]";
+
+    public final static String URGENT_NEED_CATEGORIES =
+        "[ \"Okay\", \"Needs Attention\" ]";
+
+    public final static String URGENT_NEED_DATA =
+        "[ \"1523\", \"605\" ]";
 }
--- app/src/main/java/com/dashtricks/pakistan/app/Utilities/WebAppInterface.java
+++ app/src/main/java/com/dashtricks/pakistan/app/Utilities/WebAppInterface.java
@@ -23,17 +23,6 @@ public class WebAppInterface {
         mContext = c;
     }
 
-    /**
-     * Return the json String so the webview can use it
-     *
-     * @param json String
-     * @return String json object
-     */
-    @JavascriptInterface
-    public String getJson(String json) {
-        return ""; // TODO: Replace this with Dov's stuff
-    }
-
     /** Show a toast from the web page */
     @JavascriptInterface
     public void showToast(String toast) {
@@ -46,8 +35,18 @@ public class WebAppInterface {
      * @return String json object
      */
     @JavascriptInterface
-    public String getDefaultMapsJson() {
-        return ModelDriver.requirementsAsJSON(null, null);
+    public String getSimulateMapDataString() {
+        return ModelDriver.requirementsAsJSON(null, null); // default
+    }
+
+    /**
+     * Return the json String for maps based on default
+     *
+     * @return String json object
+     */
+    @JavascriptInterface
+    public String getExploreMapDataString() {
+        return JsonObjects.EXPLORE_MAP_DATA;
     }
 
     /**
@@ -59,38 +58,67 @@ public class WebAppInterface {
      */
     @JavascriptInterface
     public String getCustomMapsJson(Facilities fs, Iterable<ImmunizationPlan> ips) {
-        return ModelDriver.requirementsAsJSON(fs, ips);
+        return ModelDriver.requirementsAsJSON(fs, ips); // pass in data
     }
 
     /**
      * Gets json for punjab bar graph
+     *
+     * @return String json object
      */
     @JavascriptInterface
-    public String getPunjabBarGraphJson() {
-      return JsonObjects.PUNJAB; 
+    public String getPunjabBarChartDataString() {
+
+        return JsonObjects.PUNJAB_DATA;
     }
 
     /**
-     * Gets json for urgent need bar graph
+     * Gets json for punjab bar graph
+     *
+     * @return String json object
+     */
+    @JavascriptInterface
+    public String getUrgentPunjabBarChartDistrictsString() {
+        return JsonObjects.PUNJAB_DISTRICTS;
+    }
+
+    /**
+     * Gets json for urgent need bar graph x axis
+     *
+     * @return String json object
      */
     @JavascriptInterface
     public String getUrgentBarChartCategoriesString() {
-        return "[ \"Okay\", \"Needs Attention\" ]";
+        return JsonObjects.URGENT_NEED_CATEGORIES;
     }
 
     /**
-     * Gets json
+     * Gets json for urgent need bar graph y axis
+     *
+     * @return String json object
      */
     @JavascriptInterface
     public String getUrgentBarChartDataString() {
-        return "[ \"1523\", \"605\" ]";
+        return JsonObjects.URGENT_NEED_DATA;
     }
 
     /**
      * Gets json for fridge age bar graph
+     *
+     * @return String json object
      */
     @JavascriptInterface
-    public String getFridgeAgeBarGraphJson() {
-      return JsonObjects.FRIDGE_AGE;
+    public String getFridgeAgeBarChartDataString() {
+      return JsonObjects.FRIDGE_AGE_DATA;
+    }
+
+    /**
+     * Gets json for fridge age bar graph
+     *
+     * @return String json object
+     */
+    @JavascriptInterface
+    public String getFridgeAgeBarChartCategoriesString() {
+        return JsonObjects.FRIDGE_AGE_CATEGORIES;
     }
 }
